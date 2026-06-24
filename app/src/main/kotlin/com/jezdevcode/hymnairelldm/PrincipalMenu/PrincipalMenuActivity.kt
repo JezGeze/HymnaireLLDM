35b7eac586882dbc435af4feb028bb782981f16f
Eliezer Bernal JezDevCode <ferrari_italia1997@hotmail.com>
2026-06-24 17:19:20 +0200

    "Este es el nuevo comienzo del proyecto ya comenzado"

---
+++ app/src/main/kotlin/com/jezdevcode/hymnairelldm/PrincipalMenu/PrincipalMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.jezdevcode.hymnairelldm
+
+import android.app.Activity
+import android.os.Bundle
+
+class PrincipalMenuActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal_menu)
+    }
+}
