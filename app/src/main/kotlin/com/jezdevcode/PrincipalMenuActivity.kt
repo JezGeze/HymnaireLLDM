6a3cff40b8b835d8e348beee871485817b30640c
Eliezer Bernal JezDevCode <ferrari_italia1997@hotmail.com>
2026-06-26 18:23:47 +0200

    "Le he añadido el cambio de activity al boton cvIndex"

--- app/src/main/kotlin/com/jezdevcode/PrincipalMenuActivity.kt
+++ app/src/main/kotlin/com/jezdevcode/PrincipalMenuActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.cardview.widget.CardView
 import android.content.Intent
 lateinit var cvHymnaire: CardView
+lateinit var cvIndex: CardView
 
 class PrincipalMenuActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,12 +18,20 @@ class PrincipalMenuActivity : Activity() {
     
     fun getViews(){
         cvHymnaire = findViewById(R.id.cv_hymnaire)
+        cvIndex = findViewById(R.id.cv_index)
     }
     
     fun buttonsAction(){
         cvHymnaire.setOnClickListener{
-            val intent = Intent(this,HymnaireListActivity::class.java)
-            startActivity(intent)
+            changeActivity(HymnaireListActivity::class.java)
         }
+        cvIndex.setOnClickListener{
+            changeActivity(HymnaireListActivity::class.java)
+        }
+    }
+    
+    fun changeActivity(activity : Class <*>){
+        val intent = Intent(this,activity)
+        startActivity(intent)
     }
 }
